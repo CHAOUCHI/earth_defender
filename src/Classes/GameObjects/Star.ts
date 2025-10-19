a6f4cf237a3f803e57d74239011107eb33a83614
CHAOUCHI <massi.chaouchi@gmail.com>
2025-10-19 21:26:16 +0200

    add star classe

---
+++ src/Classes/GameObjects/Star.ts
@@ -0,0 +1,12 @@
+import { GameObject } from "./GameObject";
+
+export class Star extends GameObject{
+
+    protected start(): void {
+        
+    }
+
+    protected update(): void {
+       
+    }
+}
